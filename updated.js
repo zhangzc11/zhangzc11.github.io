de1470574af082c642061e08d8172da2f095a07f
Zhicai <zzhang2@caltech.edu>
2023-07-08 17:13:39 -0700

    update

--- updated.js
+++ updated.js
@@ -2,12 +2,14 @@ $(function(){
     $("#header").load("header.html");
     $("#nav").load("nav.html");
     $("#footer").load("footer.html", function() {
-        fetch('https://github.com/zhangzc11/zhangzc11.github.io/commits')
-        .then(response => response.json())
-        .then(data => {
-            const lastUpdate = new Date(data[0].commit.author.date);
-            document.getElementById('last-updated').textContent = `Last updated: ${lastUpdate.toLocaleDateString()} ${lastUpdate.toLocaleTimeString()}`;
-        });
+
+        fetch('https://api.allorigins.win/raw?url=https://api.github.com/repos/zhangzc11/zhangzc11.github.io/commits')
+  .then(response => response.json())
+  .then(data => {
+    const lastUpdated = new Date(data[0].commit.author.date);
+    document.getElementById('last-updated').textContent = 'Last updated: ' + lastUpdated;
+  })
+  .catch(error => console.error(error));
     });
 });
 
